831de4906daa189d5757b4cde534cc8728d43d44
scottjehl <scott@scottjehl.com>
2011-05-24 00:49:50 -0400

    All tests are passing now in IE 6-8. More tests needed!

--- test/unit/tests.js
+++ test/unit/tests.js
@@ -28,7 +28,7 @@ window.onload = function(){
 			return testElem.offsetHeight === val;
 		}
 
-		
+		window.moveTo(0,0);
 		
 		/* TESTS HERE */ 
 		asyncTest( 'Styles not nested in media queries apply as expected', function() { 
@@ -36,7 +36,7 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( widthApplied( 50 ), "testelem is 50px wide when window is 200px wide" );
 				start();
-			}, 50);	
+			}, 900);	
 		});
 		
 		asyncTest( 'styles within min-width media queries apply properly', function() { 
@@ -44,7 +44,7 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( widthApplied( 150 ), 'testelem is 150px wide when window is 500px wide'  );
 				start();
-			}, 50);	
+			}, 900);	
 		});
 		
 		asyncTest( 'styles within max-width media queries apply properly', function() { 
@@ -52,7 +52,7 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( heightApplied( 150 ), 'testelem is 150px tall when window is under 480px wide'  );
 				start();
-			}, 50);	
+			}, 900);	
 		});
 		
 		
@@ -62,7 +62,7 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( widthApplied( 250 ), "testelem is 250px wide when window is 650px wide" );
 				start();
-			}, 50);	
+			}, 900);	
 		});
 		
 		asyncTest( "styles within a media query with a one true query among other false queries apply properly", function() { 
@@ -70,16 +70,10 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( widthApplied( 350 ), "testelem is 350px wide when window is 750px wide" );
 				start();
-			}, 50);	
+			}, 900);	
 		});
 		
-		asyncTest( "stylesheets with a media query in a media attribute apply when they should", function() { 
-			window.resizeTo(1300,600);
-			setTimeout(function(){
-				ok( widthApplied( 16 ), "testelem is 16px wide when window is 1300px wide" );
-				start();
-			}, 50);	
-		});
+		
 		
 		asyncTest( "Styles within a false media query do not apply", function() { 
 			window.resizeTo(800,600);
@@ -87,7 +81,15 @@ window.onload = function(){
 				ok( !widthApplied( 500 ), "testelem is not 500px wide when window is 1300px wide" );
 				start();
 
-			}, 50);	
+			}, 900);	
+		});
+		
+		asyncTest( "stylesheets with a media query in a media attribute apply when they should", function() { 
+			window.resizeTo(1300,600);
+			setTimeout(function(){
+				ok( widthApplied( 16 ), "testelem is 16px wide when window is 1300px wide" );
+				start();
+			}, 900);	
 		});
 		
 	}
