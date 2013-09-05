ab1c640612846be91d06a6500eeb6a3050973e6a
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2013-09-05 15:18:23 +0300

    remove unnecessary characters from CSS

--- respond.src.js
+++ respond.src.js
@@ -19,7 +19,7 @@ window.matchMedia = window.matchMedia || (function( doc, undefined ) {
 
   return function(q){
 
-    div.innerHTML = "&shy;<style media=\"" + q + "\"> #mq-test-1 { width: 42px; }</style>";
+    div.innerHTML = "&shy;<style media=\"" + q + "\">#mq-test-1{width:42px}</style>";
 
     docElem.insertBefore( fakeBody, refNode );
     bool = div.offsetWidth === 42;
