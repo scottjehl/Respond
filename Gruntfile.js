388e28d9f3c6188c3a104f9681d20bb945caee05
Jeffrey Lembeck <jlembeck@gmail.com>
2013-11-19 14:00:37 -0800

    Adding grunt, automating workflow for minification, etc.

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+	"use strict";
+
+	// Project configuration.
+	grunt.initConfig({
+		uglify: {
+			target: {
+				files: {
+					'respond.min.js': ['respond.src.js']
+				}
+			}
+		},
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: false,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				boss: true,
+				eqnull: true,
+				smarttabs: true,
+				node: true,
+				es5: true,
+				strict: false
+			},
+			globals: {
+				Image: true,
+				window: true
+			}
+		}
+	});
+
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+
+	// Default task.
+	grunt.registerTask('default', ['jshint', 'uglify:target']);
+
+};
