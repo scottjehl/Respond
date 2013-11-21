b6d657507d0d688bea46cba9a3fe0acec82b8307
Zach Leatherman <zachleatherman@gmail.com>
2013-11-21 15:53:41 -0600

    Pure whitespace commit (sorry)

--- test/unit/tests.js
+++ test/unit/tests.js
@@ -5,19 +5,19 @@
 window.onload = function(){
 
 	if( !window.opener ){
-			
+
 		document.documentElement.className = "launcher";
-			
+
 		document.getElementById("launcher").innerHTML = '<p>Tests must run in a popup window. <a href="suite.html" id="suitelink">Open test suite</a></p>';
-		
+
 		document.getElementById( "suitelink" ).onclick = function(){
 			window.open( location.href + "?" + Math.random(), 'win', 'width=800,height=600,scrollbars=1,resizable=1' );
 			return false;
 		};
-		
+
 	}
 	else {
-		
+
 		var testElem = document.getElementById("testelem");
 
 		//check if a particular style has applied properly
@@ -31,18 +31,18 @@ window.onload = function(){
 		// A short snippet for detecting versions of IE in JavaScript - author: @padolsey
 		var ie = (function(){
 
-		    var undef,
-		        v = 3,
-		        div = document.createElement('div'),
-		        all = div.getElementsByTagName('i');
-    
-		    while (
-		        div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
-		        all[0]
-		    );
-    
-		    return v > 4 ? v : undef;
-    
+				var undef,
+						v = 3,
+						div = document.createElement('div'),
+						all = div.getElementsByTagName('i');
+		
+				while (
+						div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+						all[0]
+				);
+		
+				return v > 4 ? v : undef;
+		
 		}());
 
 		window.moveTo(0,0);
@@ -53,7 +53,7 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( widthApplied( 50 ), "testelem is 50px wide when window is 200px wide" );
 				start();
-			}, 900);	
+			}, 900);
 		});
 		
 		asyncTest( 'styles within min-width media queries apply properly', function() { 
@@ -61,7 +61,7 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( widthApplied( 150 ), 'testelem is 150px wide when window is 500px wide'  );
 				start();
-			}, 900);	
+			}, 900);
 		});
 		
 		// This test is for a feature in IE7 and up
@@ -71,7 +71,7 @@ window.onload = function(){
 				setTimeout(function(){
 					ok( heightApplied( 200 ), "testelem is 200px tall when window is 500px wide" );
 					start();
-				}, 900);	
+				}, 900);
 			});
 		}
 		
@@ -81,7 +81,7 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( heightApplied( 150 ), 'testelem is 150px tall when window is under 480px wide'  );
 				start();
-			}, 900);	
+			}, 900);
 		});
 
 
@@ -91,7 +91,7 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( widthApplied( 12 ), 'testelem is 150px wide when window is 500px wide'  );
 				start();
-			}, 900);	
+			}, 900);
 		});
 		
 		
@@ -101,7 +101,7 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( widthApplied( 250 ), "testelem is 250px wide when window is 650px wide" );
 				start();
-			}, 900);	
+			}, 900);
 		});
 		
 		asyncTest( "styles within a media query with a one true query among other false queries apply properly", function() { 
@@ -109,7 +109,7 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( widthApplied( 350 ), "testelem is 350px wide when window is 750px wide" );
 				start();
-			}, 900);	
+			}, 900);
 		});
 		
 		
@@ -120,7 +120,7 @@ window.onload = function(){
 				ok( !widthApplied( 500 ), "testelem is not 500px wide when window is 800px wide" );
 				start();
 
-			}, 900);	
+			}, 900);
 		});
 		
 		asyncTest( "stylesheets with a media query in a media attribute apply when they should", function() { 
@@ -128,7 +128,7 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( widthApplied( 16 ), "testelem is 16px wide when window is 1300px wide" );
 				start();
-			}, 900);	
+			}, 900);
 		});
 		
 		asyncTest( "stylesheets with an EM-based media query in a media attribute apply when they should", function() { 
@@ -136,7 +136,7 @@ window.onload = function(){
 			setTimeout(function(){
 				ok( widthApplied( 25 ), "testelem is 25px wide when window is > 1400px wide" );
 				start();
-			}, 900);	
+			}, 900);
 		});
 		
 	}
