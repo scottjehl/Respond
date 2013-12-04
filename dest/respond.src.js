09db6a7148dfa3ba6e3199256eceff2b7b9a55c3
Zach Leatherman <zachleatherman@gmail.com>
2013-12-04 15:43:22 -0600

    Missed a few cases, also added a few tests for the issue.

--- dest/respond.src.js
+++ dest/respond.src.js
@@ -58,7 +58,7 @@
   respond.queue = requestQueue;
   respond.regex = {
     media: /@media[^\{]+\{([^\{\}]*\{[^\}\{]*\})+/gi,
-    keyframes: /@[^@]*keyframes[^\{]+\{(?:[^\{\}]*\{[^\}\{]*\})+[^\}]+\}/gi,
+    keyframes: /@(?:\-(?:o|moz|webkit)\-)?keyframes[^\{]+\{(?:[^\{\}]*\{[^\}\{]*\})+[^\}]*\}/gi,
     urls: /(url\()['"]?([^\/\)'"][^:\)'"]+)['"]?(\))/g,
     findStyles: /@media *([^\{]+)\{([\S\s]+?)$/,
     only: /(only\s+)?([a-zA-Z]+)\s?/,
