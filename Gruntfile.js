b02057aeb44e751de5e572f9ff6da6d1bcee4d22
Ivan Nikolić <niksy5@gmail.com>
2014-05-10 13:08:39 +0200

    Minify cross-domain resources

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,6 +56,32 @@ module.exports = function(grunt) {
 				files: {
 					'dest/respond.matchmedia.addListener.min.js': ['src/matchmedia.polyfill.js', 'src/matchmedia.addListener.js', 'src/respond.js']
 				}
+			},
+			crossDomain: {
+				options: {
+					preserveComments: 'some'
+				},
+				files: {
+					'dest/cross-domain/respond.proxy.min.js': ['cross-domain/respond.proxy.js']
+				}
+			}
+		},
+		htmlmin: {
+			crossDomain: {
+				options: {
+					collapseWhitespace: true,
+					conservativeCollapse: true,
+					minifyJS: true
+				},
+				files: {
+					'dest/cross-domain/respond-proxy.min.html': 'cross-domain/respond-proxy.html'
+				}
+			}
+		},
+		copy: {
+			crossDomain: {
+				src: 'cross-domain/respond.proxy.gif',
+				dest: 'dest/'
 			}
 		},
 		jshint: {
@@ -85,8 +111,10 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-htmlmin' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 
 	// Default task.
-	grunt.registerTask('default', ['jshint', 'uglify']);
+	grunt.registerTask('default', ['jshint', 'uglify', 'htmlmin', 'copy']);
 
 };
